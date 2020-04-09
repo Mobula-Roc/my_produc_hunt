fbbcc448e207bc90624e04e783d24895d5378c19
Mobula-Roc <1101553700@qq.com>
2020-04-09 21:01:22 +0800

    更新了主题样式模板

--- my_produc_hunt/settings.py
+++ my_produc_hunt/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'products/static')
+]
